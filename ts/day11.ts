812ee89e4919927e7dd1d046e9fb47add599f8cb
nate-d3v <nate.d3v@gmail.com>
2023-01-06 10:56:24 -0300

    day 11

---
+++ ts/day11.ts
@@ -0,0 +1,18 @@
+//Assigning Dynamic Keys to an Object
+const createCache = () => {
+	const cache: Record<string, string> = {};
+
+	const add = (id: string, value: string) => {
+		cache[id] = value;
+	};
+
+	const remove = (id: string) => {
+		delete cache[id];
+	};
+
+	return {
+		cache,
+		add,
+		remove,
+	};
+};
