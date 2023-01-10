b6783468506991749c18858ee3640e92fae622cb
nate-d3v <nate.d3v@gmail.com>
2023-01-10 12:58:55 -0300

    day 13

---
+++ ts/day13.ts
@@ -0,0 +1,12 @@
+//Typing Errors in a Try-Catch
+const tryCatchDemo = (state: 'fail' | 'succeed') => {
+	try {
+		if (state === 'fail') {
+			throw new Error('Failure!');
+		}
+	} catch (e) {
+		if (e instanceof Error) {
+			return e.message;
+		}
+	}
+};
