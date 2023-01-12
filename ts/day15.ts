b7e5a2e20b4846589b47bb368d4e7aca91e74803
nate-d3v <nate.d3v@gmail.com>
2023-01-12 16:33:15 -0300

    day 15

---
+++ ts/day15.ts
@@ -0,0 +1,27 @@
+//Combining Types to Create New Types
+interface User {
+	id: string;
+	firstName: string;
+	lastName: string;
+}
+
+interface Post {
+	id: string;
+	title: string;
+	body: string;
+}
+
+export const getDefaultUserAndPosts = (): User & { posts: Post[] } => {
+	return {
+		id: '1',
+		firstName: 'Matt',
+		lastName: 'Pocock',
+		posts: [
+			{
+				id: '1',
+				title: 'How I eat so much cheese',
+				body: "It's pretty edam difficult",
+			},
+		],
+	};
+};
