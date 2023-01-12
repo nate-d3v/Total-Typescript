054b79349508ea1da91b8adf3f54fbafeca44ff9
nate-d3v <nate.d3v@gmail.com>
2023-01-11 22:10:02 -0300

    day 14

---
+++ ts/day14.ts
@@ -0,0 +1,18 @@
+//Inheriting Interface Properties
+interface Base {
+	id: string;
+}
+
+interface User extends Base {
+	firstName: string;
+	lastName: string;
+}
+
+interface Post extends Base {
+	title: string;
+	body: string;
+}
+
+interface Comment extends Base {
+	comment: string;
+}
