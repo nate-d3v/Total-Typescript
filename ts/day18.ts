de315e6a5608186e3141e02a84ae44fa9d68f5d1
nate-d3v <nate.d3v@gmail.com>
2023-01-17 17:06:31 -0300

    day 18

---
+++ ts/day18.ts
@@ -0,0 +1,17 @@
+//Typing Async Functions
+interface User {
+	id: string;
+	firstName: string;
+	lastName: string;
+}
+
+const createThenGetUser = async (
+	createUser: () => Promise<string>,
+	getUser: (id: string) => Promise<User>
+): Promise<User> => {
+	const userId: string = await createUser();
+
+	const user = await getUser(userId);
+
+	return user;
+};
